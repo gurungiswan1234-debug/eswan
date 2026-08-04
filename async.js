3ea8c039dfc21ca6c3a58a4bc8b5fe1072f148f3
Eswan Tamang <gurungiswan1234@gmail.com>
2026-08-04 10:47:32 +0545

    Updated async.js

---
+++ async.js
@@ -0,0 +1,11 @@
+//timer, db querry , api call, complex algorithm
+setTimeout(()=>{
+    console.log("I will execute after 1 sec")
+}, 1000)
+
+setInterval(()=>{
+    console.log("I will execute after every one sec")
+}, 1000)
+
+console.log("I will print at last second")
+
